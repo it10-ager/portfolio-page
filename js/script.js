801062c3df462323d6f33226ebf0c8c2e992f488
German Trushkov <germantrushkov@gmail.com>
2023-03-09 13:58:01 +0200

    some update

--- js/script.js
+++ js/script.js
@@ -2,10 +2,26 @@ var lamp = document.querySelector('.lamp-light')
 var body = document.querySelector('body')
 var borderImg = document.querySelector('.avatar > img')
 var parent = document.querySelector('.skills .projects div')
+var social = document.querySelector('.contact .social')
+var saveTheme = localStorage.getItem('theme');
+
+if (saveTheme){
+	body.classList.add(saveTheme);
+	lamp.classList.toggle('white', saveTheme === 'dark');
+	borderImg.classList.toggle('border', saveTheme === 'dark');
+	parent.querySelectorAll('.container').forEach(container => container.classList.toggle('darker', saveTheme === 'dark'));
+	parent.querySelectorAll('.container .links .proj-btn').forEach(btn => btn.classList.toggle('lighter', saveTheme === 'dark'));
+	social.querySelectorAll('.contact .social div').forEach(hover => hover.classList.toggle('hover-light', saveTheme === 'dark'));
+}
 
 lamp.addEventListener('click', ()=>{
-	body.classList.toggle('white')
-	lamp.classList.toggle('orange')
+	body.classList.toggle('dark')
+	lamp.classList.toggle('white')
 	borderImg.classList.toggle('border')
-	parent.querySelectorAll('.container').forEach(container => container.classList.toggle('lighter'))
+	parent.querySelectorAll('.container').forEach(container => container.classList.toggle('darker'))
+	parent.querySelectorAll('.container .links .proj-btn').forEach(btn => btn.classList.toggle('lighter'))
+	social.querySelectorAll('.contact .social div').forEach(hover => hover.classList.toggle('hover-light'))
+	
+	var currentTheme = body.classList.contains('dark') ? 'dark' : 'light';
+	localStorage.setItem('theme', currentTheme);
 })
